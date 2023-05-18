abfbff329ab61e5bad5a169a1740b236b462adb2
vaibhav kantrodia <vaibhav.kantrodia@bytestechnolab.com>
2023-05-18 17:52:34 +0530

    add category and product from user side

--- src/api/category/category.controller.ts
+++ src/api/category/category.controller.ts
@@ -1,4 +1,16 @@
-import { Controller, Get, Post, Body, Put, Param, Delete, UseInterceptors, UploadedFile, ParseIntPipe, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFile,
+  ParseIntPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
@@ -7,30 +19,49 @@ import { FileInterceptor } from '@nestjs/platform-express';
 import CreateCategoryResponseDto from './dto/create-category-response.dto';
 import UpdateCategoryResponseDto from './dto/update-category-response.dto';
 import DeleteCategoryResponseDto from './dto/delete-category-response.dto';
+import { Category } from './entities/category.entity';
 
 
 @ApiTags('Categories')
 @Controller('categories')
 export class CategoryController {
-  constructor(private readonly categoryService: CategoryService) { }
+  constructor(private readonly categoryService: CategoryService) {}
 
   @Post('/create')
   @UseInterceptors(FileInterceptor('image_url'))
-  async create(@Body() createCategory: CreateCategoryDto, @UploadedFile() file: Express.Multer.File): Promise<CreateCategoryResponseDto> {
+  async create(
+    @Body() createCategory: CreateCategoryDto,
+    @UploadedFile() file: Express.Multer.File,
+  ): Promise<CreateCategoryResponseDto> {
     createCategory.image_url = file.filename;
     return this.categoryService.create(createCategory);
   }
 
+  @Get()
+  findAll(): Promise<Category[]> {
+    return this.categoryService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number): Promise<Category> {
+    return this.categoryService.findOne(id);
+  }
 
   @Put('/:id')
   @UseInterceptors(FileInterceptor('image_url'))
-  async update(@Param('id', ParseIntPipe) id: number, @Body() updateCategory: UpdateCategoryDto, @UploadedFile() file: Express.Multer.File): Promise<UpdateCategoryResponseDto> {
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateCategory: UpdateCategoryDto,
+    @UploadedFile() file: Express.Multer.File,
+  ): Promise<UpdateCategoryResponseDto> {
     updateCategory.image_url = file.filename;
     return this.categoryService.update(id, updateCategory);
   }
 
   @Delete('/:id')
-  async remove(@Param('id', ParseIntPipe) id: number): Promise<DeleteCategoryResponseDto> {
+  async remove(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<DeleteCategoryResponseDto> {
     return this.categoryService.remove(id);
   }
 }
--- src/api/product/product.controller.ts
+++ src/api/product/product.controller.ts
@@ -1,4 +1,17 @@
-import { Controller, Post, Body, Param, Delete, UseInterceptors, UploadedFiles, ParseIntPipe, Put, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFiles,
+  ParseIntPipe,
+  Put,
+  UseGuards,
+  Query,
+  Get,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
@@ -7,31 +20,55 @@ import { ApiTags } from '@nestjs/swagger';
 import CreateProductResponseDto from './dto/create-product-response.dto';
 import DeleteCategoryResponseDto from '../category/dto/delete-category-response.dto';
 import UpdateCategoryResponseDto from '../category/dto/update-category-response.dto';
-
+import { Product } from './entities/product.entity';
 
 @ApiTags('product')
 @Controller('products')
 export class ProductController {
-  constructor(private readonly productService: ProductService) { }
+  constructor(private readonly productService: ProductService) {}
 
   @Post('/create')
   @UseInterceptors(FilesInterceptor('product', 5))
-  async create(@Body() createProduct: CreateProductDto, @UploadedFiles() file: Array<Express.Multer.File>): Promise<CreateProductResponseDto> {
-    createProduct.image_url = file.map((val) => val.filename)
+  async create(
+    @Body() createProduct: CreateProductDto,
+    @UploadedFiles() file: Array<Express.Multer.File>,
+  ): Promise<CreateProductResponseDto> {
+    createProduct.image_url = file.map((val) => val.filename);
     return this.productService.create(createProduct);
   }
 
+  @Get()
+  async findAll(
+    @Query('name') name: string,
+    @Query('page') page: number,
+    @Query('limit') limit: number,
+  ): Promise<Product[]> {
+    return this.productService.findAll(name, page, limit);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: number): Promise<Product> {
+    return this.productService.findOne(id);
+  }
 
   @Put('/:id')
   @UseInterceptors(FilesInterceptor('product', 5))
-  async update(@Param('id', ParseIntPipe) id: number, @Body() updateProduct: UpdateProductDto, @UploadedFiles() file: Array<Express.Multer.File>): Promise<UpdateCategoryResponseDto> {
-
-    file ? updateProduct.image_url = updateProduct.image_url = file.map((val) => val.filename) : null
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProduct: UpdateProductDto,
+    @UploadedFiles() file: Array<Express.Multer.File>,
+  ): Promise<UpdateCategoryResponseDto> {
+    file
+      ? (updateProduct.image_url = updateProduct.image_url =
+          file.map((val) => val.filename))
+      : null;
     return this.productService.update(id, updateProduct);
   }
 
   @Delete('/:id')
-  async remove(@Param('id', ParseIntPipe) id: number): Promise<DeleteCategoryResponseDto> {
+  async remove(
+    @Param('id', ParseIntPipe) id: number,
+  ): Promise<DeleteCategoryResponseDto> {
     return this.productService.remove(id);
   }
 }
