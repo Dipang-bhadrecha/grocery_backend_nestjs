7a589631f99640cb8af6c7f35da1e0d085d754f1
vaibhav kantrodia <vaibhav.kantrodia@bytestechnolab.com>
2023-05-19 17:25:39 +0530

    Updated cart route

--- src/api/cart/cart.controller.ts
+++ src/api/cart/cart.controller.ts
@@ -3,13 +3,13 @@ import { CartService } from './cart.service';
 import { CreateCartDto } from './dto/create-cart.dto';
 import { UpdateCartDto } from './dto/update-cart.dto';
 
-@Controller('cart')
+@Controller('carts')
 export class CartController {
   constructor(private readonly cartService: CartService) {}
 
-  @Post()
-  create(@Body() createCartDto: CreateCartDto) {
-    return this.cartService.create(createCartDto);
+  @Post('create')
+  create(@Body() createCartDto: CreateCartDto, @GetUser() user: User) {
+    return this.cartService.create(createCartDto, user);
   }
 
   @Get()
